3456a40f27cc16cf31cd746917c9594934e0e726
Lev Izraelit <leo2002b@yahoo.com>
2019-09-23 08:47:26 -0400

    experiment

--- src/state/board/hooks.ts
+++ src/state/board/hooks.ts
@@ -1,22 +1,11 @@
-import { useReducer } from "react";
+import { useReducer, useCallback } from "react";
 import { boardReducer, defaultBoard } from "./reducer";
 import { BoardActions, SquareColors } from "./constants";
-import { Dispatch, Action } from "./reducer";
-import { all } from "q";
+import { Action } from "./reducer";
 
 type Parameters<T> = T extends (...args: infer T) => any ? T : never;
 
-type ActionDict = ({
-  [key: string]: (
-    ...args: any
-  ) => {
-    [key: string]: any;
-    type: string;
-  };
-});
-
-
-type IAction =  (
+type IAction = (
   ...args: any
 ) => {
   [key: string]: any;
@@ -31,24 +20,21 @@ const actionsDict = {
   })
 };
 
-type ProduceConditionsMapper<T> = {
-  [K in keyof T]: ((input: T[K]) => void) | Promise<T[K]>;
-};
-
-const makeDispatchableActions = <ActionNames extends string>(
+const makeDispatchableActions = (
   dispatch: React.Dispatch<any>,
-  actions: {[K in ActionNames]: IAction}
+  actions: { [key: string]: IAction }
 ) => {
-    const dispatchAbleActions:  = {...actions}
+  const dispatchAbleActions: { [key: string]: any } = {};
 
-    for (const key in actions){
-      const func = actions[key]
-      const newFunc = (...args: Parameters<typeof func>) => dispatch(func(...args))
-      dispatchAbleActions[key] = newFunc
-    }
-  
-  return dispatchAbleActions
-}
+  for (const key in actions) {
+    const func = actions[key];
+    const newFunc = (...args: Parameters<typeof func>) =>
+      dispatch(func(...args));
+    dispatchAbleActions[key] = newFunc;
+  }
+
+  return dispatchAbleActions;
+};
 
 const makeActions = (dispatch: React.Dispatch<Action>) => ({
   markBlue: (x: number, y: number) =>
@@ -62,32 +48,7 @@ const makeActions = (dispatch: React.Dispatch<Action>) => ({
 export const useBoard = () => {
   const [board, dispatch] = useReducer(boardReducer, defaultBoard);
   const actions = makeActions(dispatch);
-  const actions2 = makeDispatchableActions<"markBlue">(dispatch, actionsDict);
-  console.log(actions2);
+  const actions2 = makeDispatchableActions(dispatch, actionsDict);
+  console.log(actions2)
   return { board, ...actions };
 };
-// type Keys = "name"
-const exclamationAdder = <Keys extends string>(obj: {[k in Keys]: string}) => {
-    // type K1 = keyof v
-    const exclObj = {...obj}
-    for (const key in obj){
-      exclObj[key] = obj[key] + "!"
-    }
-
-  // const exclObj = Object.entries(obj).reduce(
-  //   (newObj, [key, value]) => ({
-  //     ...newObj,
-  //     [key]: value + "!"
-  //   }),
-  //   {}
-  // );
-  return exclObj;
-};
-
-type Keys = 'name'
-
-const obj = {
-  name: "Lev"
-}
-const withExc = exclamationAdder<Keys>(obj);
-console.log(withExc)
